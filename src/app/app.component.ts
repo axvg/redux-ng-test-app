a14c1fe6053bb9f65a09ba839beaeb878dc38049
axvg <91773368+axvg@users.noreply.github.com>
2023-06-21 17:24:58 -0500

    counter-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redux-ng-test-app';
+  counter!: number;
+
+  increaseCounter() {
+    this.counter++;
+  }
+
+  decreaseCounter() {
+    this.counter--;
+  }
+
+  constructor() {
+    this.counter = 10;
+  }
 }
