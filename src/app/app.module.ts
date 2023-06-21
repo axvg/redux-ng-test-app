4d2c103ec650e26091beba18d2a38b2cc7df09b6
axvg <91773368+axvg@users.noreply.github.com>
2023-06-21 18:50:20 -0500

    implement store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,6 +6,8 @@ import { ChildComponent } from './counter/child/child.component';
 import { GrandChildComponent } from './counter/grand-child/grand-child.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/counter.reducer';
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,15 @@ import { counterReducer } from './counter/counter.reducer';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({counter: counterReducer}, {})
+    StoreModule.forRoot({counter: counterReducer}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10,
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 30, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
