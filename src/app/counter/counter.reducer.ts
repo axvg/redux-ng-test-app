e0f203543ce26dff02ce8bec7d00b8a858656eb3
axvg <91773368+axvg@users.noreply.github.com>
2023-06-21 19:32:00 -0500

    createReducer

--- src/app/counter/counter.reducer.ts
+++ src/app/counter/counter.reducer.ts
@@ -1,13 +1,10 @@
-import { Action } from "@ngrx/store";
-import { decrement, increment } from "./counter.action";
+import { createReducer, on } from '@ngrx/store';
+import { decrement, increment } from './counter.action';
 
-export function counterReducer(state: number = 10, action: Action){
-    switch (action.type) {
-        case increment.type:
-            return state + 1;
-        case decrement.type:
-            return state - 1;
-        default:
-            return state;
-        }
-}
+export const initialState = 20;
+
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1),
+  on(decrement, (state) => state - 1)
+);
